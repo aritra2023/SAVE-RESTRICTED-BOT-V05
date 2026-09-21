74a6be54527b5d3ba9b1465502cae860229f3ce0
Aritra Mahatma <128917736+aritra2023@users.noreply.github.com>
2026-09-21 07:59:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,17 +13,17 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "29673529"))
+API_HASH = getenv("API_HASH", "36905ab3e8672eb4542b3c46a6eb0c15")
+BOT_TOKEN = getenv("BOT_TOKEN", "8774491012:AAEL_8n-gL-qjBP2Fk0G5SHyRITRXhPHTx8")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "8873984055").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://filestorebot:filestorebot@filestorebot.4y0pzna.mongodb.net/?appName=filestorebot")
+LOG_GROUP = getenv("LOG_GROUP", "-1004443016082")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1004443016082"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
-WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "50000"))
+WEBSITE_URL = getenv("WEBSITE_URL", "")
+AD_API = getenv("AD_API", "")
 STRING = getenv("STRING", None)
 YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
 DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
